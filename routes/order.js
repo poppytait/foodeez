9c4720d1731848967a8a5b0f3aa34384515f88ca
Jack Watkins <jackwatk@Jacks-MacBook-Pro.local>
2018-11-23 11:44:26 +0100

    order form get, protected route

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 
 /* GET home page. */
-router.get('/order', function (req, res, next) {
-  res.render('order', { title: 'Order' });
+router.get('/', authMiddleware.requireForOrder, (req, res, next) => {
+  res.render('order/order', { title: 'Order' });
 });
 
 module.exports = router;

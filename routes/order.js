2bb9f769dda78abe99e06f9a38e9118a95234bcb
kovitx <sllonk@gmail.com>
2018-11-24 17:31:08 +0100

    refactor order.js missing timestamp filter

--- routes/order.js
+++ routes/order.js
@@ -2,34 +2,33 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+// const User = require('../models/user');
 const Order = require('../models/order');
 const authMiddleware = require('../middlewares/authMiddleware');
 // const formMiddleware = require('../middlewares/formMiddleware');
 
 /* GET order page. */
 router.get('/', authMiddleware.requireUser, (req, res, next) => {
-  const userId = req.session.currentUser._id;
+  const userId = req.session.currentUser;
   const timestamp = new Date();
   Order.create({ userId, timestamp })
     .then(result => {
-      console.log(result);
-      res.render('order/order', { order: result });
+      res.render('order/order', { order: 'Order' });
     })
     .catch(next);
 });
 
 /* POST order page */
-
 router.post('/', authMiddleware.requireUser, (req, res, next) => {
   const { addressLine1, addressLine2, city, postcode, phoneNumber, undesiredFoodType, allergies, dietaryRequirements, budget, numberOfFoodeez } = req.body;
-  const user = User.findById(currentUser._id);
-  console.log(user);
-  const lastOrder = Order.find().sort({ timestamp: 1, userId: user });
-  // console.log(lastOrder._id);
-  Order.findByIdAndUpdate(lastOrder._id, { $set: req.body })
-    .then(() => {
-      res.redirect('/order/:id'); // Redirect to home by now
+  Order.find({ userId: req.session.currentUser }, { timestamp: 1 })
+    .then((result) => {
+      console.log(result);
+      Order.findOneAndUpdate(result._id, { $set: req.body })
+        .then(() => {
+          res.redirect('/'); // Redirect to home by now
+        })
+        .catch(next);
     })
     .catch(next);
 });

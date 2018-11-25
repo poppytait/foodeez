1c27dab2b0c2ddbede9b97af2af29cdfb9701dc7
Jack Watkins <jackwatk@Jacks-MacBook-Pro.local>
2018-11-25 15:04:32 +0100

    dont use this, its broken

--- routes/orderlist.js
+++ routes/orderlist.js
@@ -4,12 +4,20 @@ const Order = require('../models/order');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  Order.find({ userId: req.session.currentUser })
-    .then((result) => {
-      console.log(result);
-      res.render('order/orderlist', { orders: result });
-    })
-    .catch(next);
+  if (req.session.currentUser.isCustomer) {
+    Order.find({ userId: req.session.currentUser })
+      .then((result) => {
+        res.render('order/orderlist', { orders: result });
+      })
+      .catch(next);
+  } else {
+    Order.find()
+      .populate('ownerID')
+      .then((result) => {
+        res.render('order/orderlist', { orders: result });
+      })
+      .catch(next);
+  }
 });
 
 router.get('/:id', (req, res, next) => {
@@ -31,7 +39,6 @@ router.get('/:id/edit', (req, res, next) => {
 
 router.post('/:id/edit', (req, res, next) => {
   const id = req.params.id;
-  console.log(req.body);
   Order.findByIdAndUpdate(id, { $set: req.body })
     .then(() => {
       res.redirect('/orderlist');

368dbf8413f28f78df852d11fd5a6c79c3f69c72
kovitx <sllonk@gmail.com>
2018-11-26 11:19:23 +0100

    fix in login page

--- middlewares/formMiddleware.js
+++ middlewares/formMiddleware.js
@@ -3,7 +3,7 @@
 const formMiddleware = {}; // We create an empty object and then we modify it
 
 formMiddleware.requireFields = (req, res, next) => { // requireFields is the name of the function
-  if (req.path === '/signup') {
+  if (req.path === '/signup' || '/login') {
     const { email, password } = req.body;
     if (!email || !password) {
       req.flash('message-name', 'Username or password cannot be empty'); // Username and password fields can't be empty

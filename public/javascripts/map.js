e31ee530f51a0c9482d3f94f5b9007006cb05e08
kovitx <sllonk@gmail.com>
2018-11-29 13:27:16 +0100

    mapbox markers showing

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -28,7 +28,7 @@ const main = () => {
         let lat = restaurant.location.coordinates[0];
         let lng = restaurant.location.coordinates[1];
         new mapboxgl.Marker()
-          .setLngLat([lat, lng])
+          .setLngLat([lng, lat])
           .addTo(map);
       });
     })
